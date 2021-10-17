6992102f6389b2f0e8a3638baf449b4201cbf35c
NaotoshiFujita <fujita.naotoshi@gmail.com>
2021-10-17 20:15:32 +0900

    Update the Splide version.

--- dist/js/react-splide.cjs.js
+++ dist/js/react-splide.cjs.js
@@ -194,6 +194,9 @@ function style(elm, prop, value) {
 function display(elm, display2) {
   style(elm, "display", display2);
 }
+function focus(elm) {
+  elm["setActive"] && elm["setActive"]() || elm.focus({ preventScroll: true });
+}
 function getAttribute(elm, attr) {
   return elm.getAttribute(attr);
 }
@@ -1204,8 +1207,8 @@ function Move(Splide22, Components2, options) {
     return position;
   }
   function offset(index) {
-    const { focus } = options;
-    return focus === "center" ? (listSize() - slideSize(index, true)) / 2 : +focus * slideSize(index) || 0;
+    const { focus: focus2 } = options;
+    return focus2 === "center" ? (listSize() - slideSize(index, true)) / 2 : +focus2 * slideSize(index) || 0;
   }
   function getLimit(max2) {
     return toPosition(max2 ? Components2.Controller.getEnd() : 0, !!options.trimSpace);
@@ -1795,11 +1798,11 @@ function Drag(Splide22, Components2, options) {
   function coordOf(e, orthogonal) {
     return (isTouchEvent(e) ? e.touches[0] : e)[`page${resolve(orthogonal ? "Y" : "X")}`];
   }
-  function isTouchEvent(e) {
-    return typeof TouchEvent !== "undefined" && e instanceof TouchEvent;
-  }
   function timeOf(e) {
     return e.timeStamp;
+  }
+  function isTouchEvent(e) {
+    return typeof TouchEvent !== "undefined" && e instanceof TouchEvent;
   }
   function constrain(diff) {
     return diff / (hasExceeded && Splide22.is(SLIDE) ? FRICTION : 1);
@@ -1993,7 +1996,7 @@ function Pagination(Splide22, Components2, options) {
   function onClick(page) {
     Controller2.go(`>${page}`, true, () => {
       const Slide2 = Slides2.getAt(Controller2.toIndex(page));
-      Slide2 && Slide2.slide.focus();
+      Slide2 && focus(Slide2.slide);
     });
   }
   function getAt(index) {
@@ -2537,7 +2540,7 @@ var SplideSlide = ({ children: children2, className, ...props }) => {
 };
 /*!
  * Splide.js
- * Version  : 3.1.6
+ * Version  : 3.1.7
  * License  : MIT
  * Copyright: 2021 Naotoshi Fujita
  */
