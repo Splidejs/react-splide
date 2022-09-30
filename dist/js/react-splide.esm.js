9a3f69770092822dddaf15797de9bb780c79a8f0
Naotoshi Fujita <fujita.naotoshi@gmail.com>
2022-09-30 12:33:22 +0900

    Update the Splide version.

--- dist/js/react-splide.esm.js
+++ dist/js/react-splide.esm.js
@@ -63,7 +63,11 @@ function isNull(subject) {
   return subject === null;
 }
 function isHTMLElement(subject) {
-  return subject instanceof HTMLElement;
+  try {
+    return subject instanceof (subject.ownerDocument.defaultView || window).HTMLElement;
+  } catch (e) {
+    return false;
+  }
 }
 function toArray(value) {
   return isArray(value) ? value : [value];
@@ -332,9 +336,6 @@ function EventBinder() {
     destroy
   };
 }
-var ARROWS = "arrows:";
-var PAGINATION = "pagination:";
-var AUTOPLAY = "autoplay:";
 var EVENT_MOUNTED = "mounted";
 var EVENT_READY = "ready";
 var EVENT_MOVE = "move";
@@ -355,14 +356,14 @@ var EVENT_SCROLL = "scroll";
 var EVENT_SCROLLED = "scrolled";
 var EVENT_OVERFLOW = "overflow";
 var EVENT_DESTROY = "destroy";
-var EVENT_ARROWS_MOUNTED = ARROWS + "mounted";
-var EVENT_ARROWS_UPDATED = ARROWS + "updated";
-var EVENT_PAGINATION_MOUNTED = PAGINATION + "mounted";
-var EVENT_PAGINATION_UPDATED = PAGINATION + "updated";
+var EVENT_ARROWS_MOUNTED = "arrows:mounted";
+var EVENT_ARROWS_UPDATED = "arrows:updated";
+var EVENT_PAGINATION_MOUNTED = "pagination:mounted";
+var EVENT_PAGINATION_UPDATED = "pagination:updated";
 var EVENT_NAVIGATION_MOUNTED = "navigation:mounted";
-var EVENT_AUTOPLAY_PLAY = AUTOPLAY + "play";
-var EVENT_AUTOPLAY_PLAYING = AUTOPLAY + "playing";
-var EVENT_AUTOPLAY_PAUSE = AUTOPLAY + "pause";
+var EVENT_AUTOPLAY_PLAY = "autoplay:play";
+var EVENT_AUTOPLAY_PLAYING = "autoplay:playing";
+var EVENT_AUTOPLAY_PAUSE = "autoplay:pause";
 var EVENT_LAZYLOAD_LOADED = "lazyload:loaded";
 var EVENT_SLIDE_KEYDOWN = "sk";
 var EVENT_SHIFTED = "sh";
@@ -2776,7 +2777,7 @@ export {
 };
 /*!
  * Splide.js
- * Version  : 4.1.0
+ * Version  : 4.1.3
  * License  : MIT
  * Copyright: 2022 Naotoshi Fujita
  */
